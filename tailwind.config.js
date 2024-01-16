7ba6e8af0e8651978a8fd91d25df1044eb191d9e
Sumir Vats <sumirvats@gmail.com>
2024-01-16 21:44:29 +0530

    Initiated Project and added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const tailwindObj = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default tailwindObj;
